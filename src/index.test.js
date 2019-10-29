073a992c0696c56d399c36e9988511d91f5bfa4a
raythurman2386 <raymondthurman5@gmail.com>
2019-10-29 19:08:17 -0400

    added test file and initial code

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+afterEach(rtl.cleanup);
+
+describe('First Test Suite', () => {
+  it('renders without crashing', () => {
+    // ready to test!
+  });
+});
