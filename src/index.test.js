a2d301416cf83971dad4e1b8242d6029e7f05b0a
raythurman2386 <raymondthurman5@gmail.com>
2019-10-29 19:11:34 -0400

    initial test of the dashboard component

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './dashboard/Dashboard';
 
 afterEach(rtl.cleanup);
 
-describe('First Test Suite', () => {
+describe('Dashboard Suite', () => {
   it('renders without crashing', () => {
     // ready to test!
+    const wrapper = rtl.render(<Dashboard />);
+
+    wrapper.debug();
   });
 });
