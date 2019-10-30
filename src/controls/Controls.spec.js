f6d7d0ffb98929d7122cba8d9263dff0e9fc1661
raythurman2386 <raymondthurman5@gmail.com>
2019-10-29 22:00:27 -0400

    added a test to check for buttons

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -9,6 +9,12 @@ describe('Controls Suite', () => {
   it('Creates a snapshot', () => {
     // ready to test!
     const wrapper = rtl.render(<Controls />);
+    wrapper.debug();
     expect(wrapper.asFragment()).toMatchSnapshot();
+  });
+  it('Should Render the buttons', () => {
+    const wrapper = rtl.render(<Controls />);
+    const buttons = wrapper.getAllByRole('button');
+    expect(buttons.length).toBe(2);
   });
 });
