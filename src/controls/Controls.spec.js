7f0ae53147cc20494838dfa80e9fb7f409d0300b
raythurman2386 <raymondthurman5@gmail.com>
2019-10-29 21:42:29 -0400

    added a snapshot for the controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls';
+
+afterEach(rtl.cleanup);
+
+describe('Controls Suite', () => {
+  it('Creates a snapshot', () => {
+    // ready to test!
+    const wrapper = rtl.render(<Controls />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
+  });
+});
