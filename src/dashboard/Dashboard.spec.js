ca9a2b548fd29837993d65d918fd810cc56c186b
raythurman2386 <raymondthurman5@gmail.com>
2019-10-29 21:40:30 -0400

    moved tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+
+describe('Dashboard Suite', () => {
+  it('Snapshot Test', () => {
+    // ready to test!
+    const wrapper = rtl.render(<Dashboard />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
+  });
+});
