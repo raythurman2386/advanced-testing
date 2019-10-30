dadc59502e01a342dca72ad9c61750ad04fad9de
raythurman2386 <raymondthurman5@gmail.com>
2019-10-29 21:44:46 -0400

    snapshot for the display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+describe('Display Suite', () => {
+  it('Snapshot Test', () => {
+    // ready to test!
+    const wrapper = rtl.render(<Display />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
+  });
+});
